944825b50ee01d750b82aa75015e587c7df54707
thiago lopes de mello <timabuntu@gmail.com>
2022-07-15 19:45:35 -0300

    feat: tests gym

--- challenge/gym.js
+++ challenge/gym.js
@@ -5,3 +5,18 @@
 //TODO: No entanto, a função deve ser capaz de funcionar com qualquer número de alunos ou turmas
 
 //? CHALLENGE: Escreva uma função que recebe um número qualquer de números inteiros como argumentos e retorne a média aritmética entre eles.
+
+//!  TESTS:
+/*
+ * Input: (10,9,6,8,9,1,5,7)
+ * Output: 6.875
+
+*Input: (2,5,7,1,-2)
+ *Output: 2.6
+
+ * Input: (10,10,10,10,9)
+ *Output:9.8
+
+ *Input:(25,75)
+ *Output: 50
+*/
