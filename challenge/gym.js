cc48ebbea523e18e8e2abdb8ba6bc4b15955f1c6
thiago lopes de mello <timabuntu@gmail.com>
2022-07-15 19:30:48 -0300

    feat: created description gym

---
+++ challenge/gym.js
@@ -0,0 +1,5 @@
+// todo:  A academia de Oficiais da Federação precisa da sua ajuda para criar um programa que automatize os cálculos das médias de alunos e turmas para melhor acompanhar os seus desempenhos.
+
+// todo: Este programa será incorporado em seu sistema principal, portanto você precisa apenas desenvolver a função que calcula a média.
+
+//todo: No entanto, a função deve ser capaz de funcionar com qualquer número de alunos ou turmas
