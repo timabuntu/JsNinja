5884194dddd264343b174f8d7609927033452894
thiago lopes de mello <timabuntu@gmail.com>
2022-07-15 19:34:27 -0300

    feat: challenge gym

--- challenge/gym.js
+++ challenge/gym.js
@@ -1,5 +1,7 @@
-// todo:  A academia de Oficiais da Federação precisa da sua ajuda para criar um programa que automatize os cálculos das médias de alunos e turmas para melhor acompanhar os seus desempenhos.
+// TODO:  A academia de Oficiais da Federação precisa da sua ajuda para criar um programa que automatize os cálculos das médias de alunos e turmas para melhor acompanhar os seus desempenhos.
 
-// todo: Este programa será incorporado em seu sistema principal, portanto você precisa apenas desenvolver a função que calcula a média.
+// TODO: Este programa será incorporado em seu sistema principal, portanto você precisa apenas desenvolver a função que calcula a média.
 
-//todo: No entanto, a função deve ser capaz de funcionar com qualquer número de alunos ou turmas
+//TODO: No entanto, a função deve ser capaz de funcionar com qualquer número de alunos ou turmas
+
+//? CHALLENGE: Escreva uma função que recebe um número qualquer de números inteiros como argumentos e retorne a média aritmética entre eles.
