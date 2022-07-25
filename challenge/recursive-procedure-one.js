976a07c381c48a4237900f6d73f9294338ff2c3e
thiago lopes de mello <timabuntu@gmail.com>
2022-07-25 09:24:42 -0300

    feat: created description recursive procedure one

---
+++ challenge/recursive-procedure-one.js
@@ -0,0 +1,22 @@
+// TODO:  A equipe de manutenção da nave HighWind solicitou a sua ajuda para analisar o desempenho do computador portátil utilizado pela equipe de reconhecimento.
+
+// TODO: Para isso, em um determinado momento, você precisou simular a criação de blocos de informação em formato de texto recursivamente
+
+//TODO: Então decidiu criar uma função para imitar esse comportamento
+
+//? CHALLENGE: Escreva uma função que recebe um número e retorna uma quantidade equivalente de "chunks" separados por um traço "-" sem utilizar nenhuma estrutura de repetição(while, dowhile, for,etc...).
+
+//!  TESTS:
+/*
+ * Input: (4)
+ * Output: "chunk-chunk-chunk-chunk"
+
+*Input: (1)
+ *Output: "chunk"
+
+ * Input: (8)
+ *Output:"chunk-"
+
+ *Input:(2)
+ *Output: "chunk-chunk"
+*/
