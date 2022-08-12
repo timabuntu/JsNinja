8ccb5c6085862aefb8fb5b7c2de1022ea6d14256
thiago lopes de mello <timabuntu@gmail.com>
2022-08-12 10:31:12 -0300

    feat: learned array

--- programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/04-arrays/main.js
+++ programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/04-arrays/main.js
@@ -1,1 +1,15 @@
-console.log("oi");
+//? Arrays
+let heroes = ["Batman", "Superman", "CatWoman", "IronMan"];
+const ages = [32, 23, 39, 45, 23];
+const randomArray = ["Parkey", "Diana", 19, 18];
+
+console.log(heroes[1]);
+console.log(heroes[3 - 1]);
+console.log(ages[2]);
+
+//* Tipos diferentes de dados
+console.log(randomArray);
+
+//* Atribuir "Sobrescrever" valor a um array
+heroes[0] = "Flash";
+console.log(heroes[0]);
