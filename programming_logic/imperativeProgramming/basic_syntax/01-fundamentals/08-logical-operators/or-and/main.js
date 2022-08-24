ad2a2ba80d16a62a69bbc3c5043c4abca6f8a700
thiago lopes de mello <timabuntu@gmail.com>
2022-08-24 11:14:51 -0300

    feat: learned "or" ||  "and" &&

--- programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/08-logical-operators/or-and/main.js
+++ programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/08-logical-operators/or-and/main.js
@@ -1,1 +1,14 @@
 // operadores lógicos "OU" || e "E" &&
+const password = "oiasdfjklç23";
+
+//bad practices
+if (password.length >= 12 && password.includes("1")) {
+  console.log("Senha muito forte 😃");
+} else if (
+  password.length >= 8 ||
+  (password.includes("1") && password.length >= 5)
+) {
+  console.log("senha forte 👍");
+} else {
+  console.log("A senha deve conter 8 ou mais caracteres");
+}
