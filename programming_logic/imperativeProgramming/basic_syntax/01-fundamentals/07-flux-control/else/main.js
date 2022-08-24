6b73da012a2e08c9ac8fa6ea60927aad71bd6fd8
thiago lopes de mello <timabuntu@gmail.com>
2022-08-24 10:58:01 -0300

    feat: learned else and else if

--- programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/07-flux-control/else/main.js
+++ programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/07-flux-control/else/main.js
@@ -1,1 +1,10 @@
 // else e else if statements
+const password = "oi1234";
+
+if (password.length >= 12) {
+  console.log("Senha muito forte 😃");
+} else if (password.length >= 8) {
+  console.log("Essa senha tem 8 ou mais caracteres");
+} else {
+  console.log("A senha deve conter 8 ou mais caracteres");
+}
