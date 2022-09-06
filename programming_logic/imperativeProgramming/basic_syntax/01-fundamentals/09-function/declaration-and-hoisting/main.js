7143ced3d98d3830f80de4e5da4e7cb26b6ef3fe
thiago lopes de mello <timabuntu@gmail.com>
2022-09-06 15:55:50 -0300

    feat: learned function declaration and hoisting

--- programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/09-function/declaration-and-hoisting/main.js
+++ programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/09-function/declaration-and-hoisting/main.js
@@ -1,3 +1,16 @@
 // Function declaration
+function sayHi() {
+  console.log("oi");
+}
+
+sayHi();
+sayHi();
 
 // Function expression
+const showFood = function () {
+  console.log("pizza");
+};
+
+showFood();
+showFood();
+showFood();
