36e740c3d3b648bd45d1f3ff1df9a5031f851503
thiago lopes de mello <timabuntu@gmail.com>
2022-09-08 09:24:48 -0300

    feat: learned argument and parameter

--- programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/09-function/argument-and-parameter/main.js
+++ programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/09-function/argument-and-parameter/main.js
@@ -1,1 +1,13 @@
 // Argumentos, Parâmetros e default parameters
+const myName = function (name) {
+  console.log(`Oi,${name}!`);
+};
+
+myName("Thiago");
+
+const fullName = function (name = "José", lastName = "da Silva") {
+  console.log(`Seu nome completo é: ${name} ${lastName}`);
+};
+
+fullName("Thiago", "Lopes de Mello");
+fullName("John doe");
