d626c6c3ed740d23c92453d97e0a92728cf10993
thiago lopes de mello <timabuntu@gmail.com>
2022-09-19 10:13:07 -0300

    feat: learned arrow function

--- programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/09-function/arrow-function/main.js
+++ programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/09-function/arrow-function/main.js
@@ -1,1 +1,14 @@
-// Arrow function
+// Função tradicional
+const double = function (number) {
+  return number * 2;
+};
+
+const result = double(4);
+console.log(`O resultado é: ${result}`);
+
+//Arrow function
+
+const doubleArrow = (number) => number * 2;
+
+const resultArrow = doubleArrow(6);
+console.log(`O resultado é: ${resultArrow}`);
