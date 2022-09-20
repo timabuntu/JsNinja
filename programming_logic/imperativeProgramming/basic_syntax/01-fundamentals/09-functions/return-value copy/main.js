8b440b41a88979d42450947b0ca1aaaf66776cdd
thiago lopes de mello <timabuntu@gmail.com>
2022-09-20 13:35:28 -0300

    feat: learned forEach and callbacks

--- programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/09-functions/return-value copy/main.js
+++ programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/09-functions/return-value copy/main.js
@@ -1,1 +1,20 @@
 // forEach e callbacks
+
+// Callbacks
+const myFunc = (callback) => {
+  const value = 77;
+
+  callback(value);
+};
+
+myFunc((number) => {
+  console.log(number);
+});
+
+// forEach
+const socialNetworks = ["youtube", "twitter", "instagram", "facebook"];
+
+const logArrayInfo = (socialNetworks, index, array) => {
+  console.log(index, socialNetworks, array);
+};
+socialNetworks.forEach(logArrayInfo);
