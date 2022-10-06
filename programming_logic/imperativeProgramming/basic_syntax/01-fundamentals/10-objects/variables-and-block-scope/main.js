0bd187a05bbab6a8248b8f87dbc31cbea98152b7
thiago lopes de mello <timabuntu@gmail.com>
2022-10-06 09:50:04 -0300

    feat: learned variables and block-scope

--- programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/10-objects/variables-and-block-scope/main.js
+++ programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/10-objects/variables-and-block-scope/main.js
@@ -1,1 +1,19 @@
 // Variáveis e escopo de bloco
+const age = 39;
+
+if (true) {
+  const age = 41;
+  const name = "Thiago";
+
+  console.log(`dentro do 1° bloco de código: ${age} ${name}`);
+
+  if (true) {
+    const age = 51;
+    console.log(`dentro do 2° bloco de código: ${age}`);
+
+    var test = "oi";
+  }
+}
+
+console.log(`fora do bloco de código: ${age} e o problema com var ${test}`);
+console.log(`tentando acessar ${name}`);
