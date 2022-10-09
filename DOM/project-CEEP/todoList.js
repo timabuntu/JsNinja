a8952c4ed81afc6de6ee0902bc5ff5694d000e7f
thiago lopes de mello <timabuntu@gmail.com>
2022-10-09 15:20:01 -0300

    chore: refactored code

--- DOM/project-CEEP/todoList.js
+++ DOM/project-CEEP/todoList.js
@@ -1,9 +1,12 @@
-const newTask = document.querySelector("[data-form-button]");
-
-newTask.addEventListener("click", (event) => {
+const createTask = (event) => {
   event.preventDefault();
 
   const input = document.querySelector("[data-form-input]");
   const value = input.value;
+
   console.log(value);
-});
+};
+
+const newTask = document.querySelector("[data-form-button]");
+
+newTask.addEventListener("click", createTask);
