9a0c2a8be60deb91a2f4f8cb1ee6da1ea6085226
thiago lopes de mello <timabuntu@gmail.com>
2022-10-09 11:46:48 -0300

    feat: selected input

--- DOM/project-CEEP/todoList.js
+++ DOM/project-CEEP/todoList.js
@@ -1,5 +1,7 @@
 const newTask = document.querySelector("[data-form-button]");
+const input = document.querySelector("[data-form-input]");
 
+console.log(input);
 newTask.addEventListener("click", () => {
   console.log("Fui clicado");
 });
