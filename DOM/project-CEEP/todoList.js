9ea576c99d18c2a3e844b957f30c5e7934ca6a7d
thiago lopes de mello <timabuntu@gmail.com>
2022-10-10 10:25:25 -0300

    feat: created li element

--- DOM/project-CEEP/todoList.js
+++ DOM/project-CEEP/todoList.js
@@ -4,7 +4,9 @@ const createTask = (event) => {
   const input = document.querySelector("[data-form-input]");
   const value = input.value;
 
+  const task = document.createElement("li");
   const templateHTML = `<p class="content">${value}</>`;
+  task.innerHTML = templateHTML;
 
   input.value = " ";
 };
