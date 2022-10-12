c52e06e50a836824fcb4d9c96d32d86c0aa315bc
thiago lopes de mello <timabuntu@gmail.com>
2022-10-12 12:06:24 -0300

    feat: separate ButtonDone component

---
+++ DOM/project-CEEP/components/ButtonDone.js
@@ -0,0 +1,16 @@
+const ButtonDone = () => {
+  const finishButton = document.createElement("button");
+
+  finishButton.classList.add("check-button");
+  finishButton.innerText = "Concluir";
+  finishButton.addEventListener("click", completeTask);
+
+  return finishButton;
+};
+
+const completeTask = (event) => {
+  const buttonDone = event.target;
+
+  const completeTask = buttonDone.parentElement;
+  completeTask.classList.toggle("done");
+};
