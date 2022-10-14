11fa97a27755dc2904c92721867d1d65b642fcb0
thiago lopes de mello <timabuntu@gmail.com>
2022-10-14 10:53:06 -0300

    feat: learned class behavior

--- programming_logic/objectOriented/bitBank/index.js
+++ programming_logic/objectOriented/bitBank/index.js
@@ -1,29 +1,45 @@
 class Cliente {
   nome;
   cpf;
+}
+
+class ContaCorrente {
   agencia;
   saldo;
+
+  sacar(valor) {
+    if (this.saldo >= valor) {
+      this.saldo -= valor;
+    }
+  }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
-
 cliente1.nome = "Thiago";
 cliente1.cpf = 111222333444;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Fernanda";
 cliente2.cpf = 999888777666;
-cliente2.agencia = 1001;
-cliente2.saldo = 2000;
 
-cliente3.nome = "Carol";
-cliente3.cpf = 111999222888;
-cliente3.agencia = 2002;
-cliente3.saldo = 50;
+const contaCorrenteThiago = new ContaCorrente();
+contaCorrenteThiago.saldo = 0;
+contaCorrenteThiago.agencia = 1001;
+
+contaCorrenteThiago.saldo = 100;
+console.log(contaCorrenteThiago.saldo);
+contaCorrenteThiago.sacar(70);
 
 console.log(cliente1);
+console.log(contaCorrenteThiago.saldo);
+
+const contaCorrenteFernanda = new ContaCorrente();
+contaCorrenteFernanda.saldo = 0;
+contaCorrenteFernanda.agencia = 2002;
+
+contaCorrenteFernanda.saldo = 200;
+console.log(contaCorrenteFernanda.saldo);
+contaCorrenteFernanda.sacar(250);
+
 console.log(cliente2);
-console.log(cliente3);
+console.log(contaCorrenteFernanda.saldo);
