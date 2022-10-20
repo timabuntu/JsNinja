96bb7761d3a1be16a0f28997ba6dc4341f66a2d1
thiago lopes de mello <timabuntu@gmail.com>
2022-10-20 09:52:24 -0300

    chore: organized logic

--- programming_logic/objectOriented/bitBank/index.js
+++ programming_logic/objectOriented/bitBank/index.js
@@ -29,26 +29,21 @@ const contaCorrenteThiago = new ContaCorrente();
 contaCorrenteThiago.saldo = 0;
 contaCorrenteThiago.agencia = 1001;
 
-contaCorrenteThiago.saldo = 100;
-console.log(contaCorrenteThiago.saldo);
 contaCorrenteThiago.sacar(70);
 contaCorrenteThiago.depositar(300);
 
-console.log(cliente1);
-console.log(contaCorrenteThiago.saldo);
+console.log(contaCorrenteThiago);
 
 // CLIENTE 2*************************************************************
 const cliente2 = new Cliente();
 cliente2.nome = "Fernanda";
 cliente2.cpf = 999888777666;
+
 const contaCorrenteFernanda = new ContaCorrente();
 contaCorrenteFernanda.saldo = 0;
 contaCorrenteFernanda.agencia = 2002;
 
-contaCorrenteFernanda.saldo = 200;
-console.log(contaCorrenteFernanda.saldo);
 contaCorrenteFernanda.sacar(250);
 contaCorrenteFernanda.depositar(50);
 
-console.log(cliente2);
-console.log(contaCorrenteFernanda.saldo);
+console.log(contaCorrenteFernanda);
