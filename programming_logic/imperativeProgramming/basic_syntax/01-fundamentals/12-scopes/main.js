02e35e1b1ae65e9fbab64354e6883b5afdb6a31c
thiago lopes de mello <timabuntu@gmail.com>
2022-10-26 15:56:38 -0300

    feat: learned function scope

--- programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/12-scopes/main.js
+++ programming_logic/imperativeProgramming/basic_syntax/01-fundamentals/12-scopes/main.js
@@ -1,1 +1,29 @@
 // Escopos
+
+// Escopo de funções
+
+function myFunc() {
+  let dog = "Bidu";
+  const age = 3;
+  var color = "gray";
+  console.log(age);
+}
+
+function myFunc2() {
+  var color = "white";
+  const age = 5;
+  console.log(age);
+}
+
+myFunc();
+myFunc2();
+
+const cat = "Persa";
+
+function catWatch() {
+  const cat = "Siamês";
+  console.log(cat);
+}
+
+catWatch();
+console.log(cat);
