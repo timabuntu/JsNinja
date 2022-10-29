591ae5df4e1223a4b880808b06f5bd9382c82770
thiago lopes de mello <timabuntu@gmail.com>
2022-10-29 12:04:17 -0300

    feat: created static attributes

--- programming_logic/objectOriented/byteBank/index.js
+++ programming_logic/objectOriented/byteBank/index.js
@@ -4,8 +4,10 @@ import { ContaCorrente } from "./ContaCorrente.js";
 // CLIENTES *************************************************************
 const cliente1 = new Cliente("Thiago", 111222333444);
 const cliente2 = new Cliente("Fernanda", 999888777666);
-console.log(cliente2.cpf);
+// console.log(cliente2.cpf);
 
 const contaCorrenteThiago = new ContaCorrente(1001, cliente1);
 contaCorrenteThiago.depositar(500);
 const contaCorrenteFernanda = new ContaCorrente(2002, cliente2);
+
+console.log(ContaCorrente.numeroDeContas);
