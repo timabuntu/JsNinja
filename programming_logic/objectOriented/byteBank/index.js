bad63cc5bb58188d3b72a26a144f4764bc3ef509
thiago lopes de mello <timabuntu@gmail.com>
2022-10-29 00:45:49 -0300

    chore: reorganized code

--- programming_logic/objectOriented/byteBank/index.js
+++ programming_logic/objectOriented/byteBank/index.js
@@ -1,18 +1,15 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-// CLIENTE 1*************************************************************
+// CLIENTES *************************************************************
 const cliente1 = new Cliente("Thiago", 111222333444);
+const cliente2 = new Cliente("Fernanda", 999888777666);
+console.log(cliente2.cpf);
 
 const contaCorrenteThiago = new ContaCorrente();
 contaCorrenteThiago.agencia = 1001;
 contaCorrenteThiago.cliente = cliente1;
 
-// CLIENTE 2*************************************************************
-const cliente2 = new Cliente("Fernanda", 999888777666);
-
 const contaCorrenteFernanda = new ContaCorrente();
 contaCorrenteFernanda.agencia = 2002;
 contaCorrenteFernanda.cliente = cliente2;
-
-console.log(cliente2);
