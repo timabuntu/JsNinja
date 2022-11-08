fedf06561aba16d981a3d223002c392fab4e70a3
thiago lopes de mello <timabuntu@gmail.com>
2022-11-08 10:51:20 -0300

    feat: pokemon list displayed in html

--- projects/POKEDEX/assets/js/main.js
+++ projects/POKEDEX/assets/js/main.js
@@ -21,14 +21,15 @@ function convertPokemonToLi(pokemon) {
       </li>
   `;
 }
+const pokemonList = document.querySelector("#pokemonList");
 
 fetch(url)
   .then((response) => response.json())
   .then((jsonBody) => jsonBody.results)
-  .then((pokemonList) => {
-    for (let i = 0; i < pokemonList.length; i++) {
-      const pokemon = pokemonList[i];
-      console.log(convertPokemonToLi(pokemon));
+  .then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i];
+      pokemonList.innerHTML += convertPokemonToLi(pokemon);
     }
   })
   .catch((error) => console.log(error));
