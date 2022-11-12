029581bcc7cfe412246fc8f77009b0a957c9506d
thiago lopes de mello <timabuntu@gmail.com>
2022-11-12 11:08:08 -0300

    feat: created multiple requests in parallel

--- projects/POKEDEX/assets/js/main.js
+++ projects/POKEDEX/assets/js/main.js
@@ -1,16 +1,21 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes.map(
+    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
+  );
+}
+
 function convertPokemonToLi(pokemon) {
   return `
         <li class="pokemon">
-          <span class="number">#001</span>
+          <span class="number">#${pokemon.order}</span>
           <span class="name">${pokemon.name}</span>
 
           <div class="detail">
             <ol class="types">
-              <li class="type">poison</li>
-              <li class="type">grass</li>
+              ${convertPokemonTypesToLi(pokemon.types).join("")}
             </ol>
             <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+              src="${pokemon.sprites.other.dream_world.front_default}"
               alt='${pokemon.name}'
             />
           </div>
