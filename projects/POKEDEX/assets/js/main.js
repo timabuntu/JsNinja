538afcd3847753890c97e8d48ecdc0142bb0d208
thiago lopes de mello <timabuntu@gmail.com>
2022-11-11 23:45:20 -0300

    chore: refactor-list-pokemons

--- projects/POKEDEX/assets/js/main.js
+++ projects/POKEDEX/assets/js/main.js
@@ -24,12 +24,12 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.querySelector("#pokemonList");
 
-pokeApi.getPokemons().then((pokemons) => {
-  const listItems = [];
+pokeApi.getPokemons().then((pokemons = []) => {
+  const newList = pokemons.map((pokemon) => {
+    return convertPokemonToLi(pokemon);
+  });
 
-  for (let i = 0; i < pokemons.length; i++) {
-    const pokemon = pokemons[i];
-    listItems.push(convertPokemonToLi(pokemon));
-  }
-  console.log(listItems);
+  const newHtml = newList.join("");
+
+  pokemonList.innerHTML += newHtml;
 });
