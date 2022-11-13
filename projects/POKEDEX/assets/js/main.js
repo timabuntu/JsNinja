13b6787b01cd83eb87b117acc4d4d5ff658f15fa
thiago lopes de mello <timabuntu@gmail.com>
2022-11-13 11:17:12 -0300

    feat: created colors type pokemon

--- projects/POKEDEX/assets/js/main.js
+++ projects/POKEDEX/assets/js/main.js
@@ -1,6 +1,6 @@
 function convertPokemonToLi(pokemon) {
   return `
-        <li class="pokemon">
+        <li class="pokemon ${pokemon.type}">
           <span class="number">#${pokemon.number}</span>
           <span class="name">${pokemon.name}</span>
 
