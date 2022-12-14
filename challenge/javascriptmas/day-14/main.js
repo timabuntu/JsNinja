7f9ab50cd5cc16bf21de246b63d3449ba762e2f1
thiago lopes de mello <timabuntu@gmail.com>
2022-12-13 21:24:05 -0300

    feat: created day 14

---
+++ challenge/javascriptmas/day-14/main.js
@@ -0,0 +1,25 @@
+function countVowelConsonant(str) {
+  // write code here
+
+}
+
+
+
+/**
+* Test Suite
+*/
+describe('countVowelConsonant()', () => {
+    it('returns total of vowels(1) and consonants(2) to be added', () => {
+        // arrange
+        const value = 'abcde';
+
+        // act
+        const result = countVowelConsonant(value);
+
+        // log
+        console.log("result: ", result);
+
+        // assert
+        expect(result).toBe(8);
+    });
+});
